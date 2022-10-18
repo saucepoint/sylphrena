fd9f0a4d200a6563abe7b034759a69f57aaf73bd
saucepoint <saucepoint@protonmail.com>
2022-10-18 15:49:12 -0400

    jam

---
+++ src/Spren.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Spren {
+    address immutable stormFather;
+    constructor(address _stormFather) {
+        stormFather = _stormFather;
+    }
+
+    function unbond(address oathBreaker) external {
+        require(msg.sender == stormFather, "only storm father can unbond");
+        selfdestruct(payable(oathBreaker));
+    }
+}
